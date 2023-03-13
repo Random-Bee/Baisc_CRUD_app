ce0922f9b327effdee49fbfcdf7e9ef04b5e5da0
Random-Bee <co21btech11002@iith.ac.in>
2023-03-13 10:21:08 +0530

    changed database

--- back/index.js
+++ back/index.js
@@ -5,17 +5,11 @@ const sql = require('mysql2');
 const cors = require('cors');
 const pass = require('./password');
 
-// const pool = sql.createPool({
-//     host: 'sql12.freemysqlhosting.net',
-//     user: 'sql12604744',
-//     password: pass,
-//     database: 'sql12604744',
-// });
 const pool = sql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: "My_4rec1ou5",
-    database: 'crud_contact',
+    host: 'sql12.freemysqlhosting.net',
+    user: 'sql12604744',
+    password: pass,
+    database: 'sql12604744',
 });
 
 app.use(cors());
@@ -23,8 +17,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/get', (req, res) => {
-    const sqlSelect = "SELECT * FROM contact_db";
-    // const sqlSelect = "SELECT * FROM Users";
+    const sqlSelect = "SELECT * FROM Users";
     pool.query(sqlSelect, (err, result) => {
         res.send(result);
     });
@@ -32,8 +25,7 @@ app.get('/get', (req, res) => {
 
 app.get('/get/:id', (req, res) => {
     const {id} = req.params;
-    const sqlSelect = "SELECT * FROM contact_db where id = ?";
-    // const sqlSelect = "SELECT * FROM Users where id = ?";
+    const sqlSelect = "SELECT * FROM Users where id = ?";
     pool.query(sqlSelect, id, (err, result) => {
         res.send(result);
     });
@@ -43,27 +35,23 @@ app.post('/post', (req, res) => {
     const first_name = req.body.fname;
     const last_name = req.body.lname;
     const email = req.body.email;
-    const sqlInsert = "INSERT INTO contact_db (First_Name, Last_Name, Email) VALUES (?,?,?)";
-    // const sqlInsert = "INSERT INTO Users (First_Name, Last_Name, Email) VALUES (?,?,?)";
+    const sqlInsert = "INSERT INTO Users (First_Name, Last_Name, Email) VALUES (?,?,?)";
     pool.query(sqlInsert, [first_name, last_name, email]);
 })
 
 app.post('/delete/:id', (req, res) => {
     const {id} = req.params;
-    console.log(req.params);
     const sqlDelete = "DELETE FROM contact_db WHERE ID = ?";
     // const sqlDelete = "DELETE FROM Users WHERE ID = ?";
     pool.query(sqlDelete, id);
 })
 
 app.put('/update/:id', (req, res) => {
-    console.log(req.params, req.body);
     const {id} = req.params;
     const first_name = req.body.fname;
     const last_name = req.body.lname;
     const email = req.body.email;
-    const sqlUpdate = "UPDATE contact_db SET First_Name = ?, Last_Name = ?, Email = ? WHERE ID = ?";
-    // const sqlUpdate = "UPDATE Users SET First_Name = ?, Last_Name = ?, Email = ? WHERE ID = ?";
+    const sqlUpdate = "UPDATE Users SET First_Name = ?, Last_Name = ?, Email = ? WHERE ID = ?";
     pool.query(sqlUpdate, [first_name, last_name, email, id]);
 })
 
