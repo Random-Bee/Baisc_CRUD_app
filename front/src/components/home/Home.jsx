3331b47cdc210a3e30a94808d143886fbfd14249
Random-Bee <co21btech11002@iith.ac.in>
2023-03-13 20:05:11 +0530

    Delete Button

--- front/src/components/home/Home.jsx
+++ front/src/components/home/Home.jsx
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom"
 import { toast } from "react-toastify"
 import axios from "axios"
 import './home.css'
-import { Container, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Button, Box } from '@mui/material'
+import { Slide, Container, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Button, Box, Dialog, DialogActions, DialogTitle, DialogContent, DialogContentText } from '@mui/material'
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const Home = () => {
   const [data, setData] = useState([]);
@@ -17,50 +21,90 @@ const Home = () => {
     loadData();
   }, []);
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
   const deleteContact = (id) => {
-    if (window.confirm("Are you sure you want to delete this contact?")) {
+    // if (window.confirm("Are you sure you want to delete this contact?")) {
       axios.post(`http://localhost:5000/delete/${id}`);
       toast.success("Contact deleted successfully");
       setTimeout(() => loadData(), 500);
-    }
+    // }
+  }
+
+  const agree = (id) => {
+    deleteContact(id);
+    handleClose();
   }
 
   return (
     <Container>
-      <Box className='Home scale-up-center' style={{padding: '1rem'}}>
-        <TableContainer component={Paper} sx={{ maxHeight: '400px'}} >
+      <Box className='Home scale-up-center' style={{ padding: '1rem' }}>
+        <TableContainer component={Paper} sx={{ maxHeight: '400px' }} >
           <Table aria-label='User Info' stickyHeader>
             <TableHead>
               <TableRow>
-                <TableCell align='center'style={{ padding:'1rem'}} sx={{bgcolor:'black', color:'white', fontWeight:'bold', fontSize:'1rem'}}>ID</TableCell>
-                <TableCell align='center'style={{ padding:'1rem'}} sx={{bgcolor:'black', color:'white', fontWeight:'bold', fontSize:'1rem'}}>First Name</TableCell>
-                <TableCell align='center'style={{ padding:'1rem'}} sx={{bgcolor:'black', color:'white', fontWeight:'bold', fontSize:'1rem'}}>Last Name</TableCell>
-                <TableCell align='center'style={{ padding:'1rem'}} sx={{bgcolor:'black', color:'white', fontWeight:'bold', fontSize:'1rem'}}>Email</TableCell>
-                <TableCell align='center'style={{ padding:'1rem'}} sx={{bgcolor:'black', color:'white', fontWeight:'bold', fontSize:'1rem'}}>Action</TableCell>
+                <TableCell align='center' style={{ padding: '1rem' }} sx={{ bgcolor: 'black', color: 'white', fontWeight: 'bold', fontSize: '1rem' }}>ID</TableCell>
+                <TableCell align='center' style={{ padding: '1rem' }} sx={{ bgcolor: 'black', color: 'white', fontWeight: 'bold', fontSize: '1rem' }}>First Name</TableCell>
+                <TableCell align='center' style={{ padding: '1rem' }} sx={{ bgcolor: 'black', color: 'white', fontWeight: 'bold', fontSize: '1rem' }}>Last Name</TableCell>
+                <TableCell align='center' style={{ padding: '1rem' }} sx={{ bgcolor: 'black', color: 'white', fontWeight: 'bold', fontSize: '1rem' }}>Email</TableCell>
+                <TableCell align='center' style={{ padding: '1rem' }} sx={{ bgcolor: 'black', color: 'white', fontWeight: 'bold', fontSize: '1rem' }}>Action</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {data.map((item, index) => {
                 return (
-                  <TableRow key={item.ID}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell align='center'>{index + 1}</TableCell>
-                    <TableCell align='center'>{item.First_Name}</TableCell>
-                    <TableCell align='center'>{item.Last_Name}</TableCell>
-                    <TableCell align='center'>{item.Email}</TableCell>
-                    <TableCell align='center'>
-                      <Box className='btn-columns'>
-                        <Link to={`/update/${item.ID}`} style={{ textDecoration: 'none' }}>
-                          <Button className='Home-btn-btn' variant='contained' color='secondary'>Edit</Button>
-                        </Link>
-                        <Button className='Home-btn-btn' onClick={() => deleteContact(item.ID)} variant='contained' color='error'>Delete</Button>
-                        <Link to={`/info/${item.ID}`} style={{ textDecoration: 'none' }}>
-                          <Button className='Home-btn-btn' variant='contained'>Get</Button>
-                        </Link>
-                      </Box>
-                    </TableCell>
-                  </TableRow>
+                  <>
+                    <TableRow key={item.ID}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell align='center'>{index + 1}</TableCell>
+                      <TableCell align='center'>{item.First_Name}</TableCell>
+                      <TableCell align='center'>{item.Last_Name}</TableCell>
+                      <TableCell align='center'>{item.Email}</TableCell>
+                      <TableCell align='center'>
+                        <Box className='btn-columns'>
+                          <Link to={`/update/${item.ID}`} style={{ textDecoration: 'none' }}>
+                            <Button className='Home-btn-btn' variant='contained' color='secondary'>Edit</Button>
+                          </Link>
+                          <Button variant="contained" onClick={handleClickOpen} color='error'>Delete</Button>
+
+                          <Link to={`/info/${item.ID}`} style={{ textDecoration: 'none' }}>
+                            <Button className='Home-btn-btn' variant='contained'>Get</Button>
+                          </Link>
+                        </Box>
+                      </TableCell>
+                    </TableRow>
+
+                    <Dialog
+                      open={open}
+                      TransitionComponent={Transition}
+                      keepMounted
+                      onClose={handleClose}
+                      aria-describedby="alert-dialog-slide-description"
+                      BackdropProps={{ style: { backgroundColor: 'rgba(0, 0, 0, 0.5)' } }}
+                    >
+                      <DialogTitle>{"Delete Contact?"}</DialogTitle>
+                      <DialogContent>
+                        <DialogContentText id="alert-dialog-slide-description">
+                          Are you sure you want to delete this contact?
+                        </DialogContentText>
+                      </DialogContent>
+                      <DialogActions>
+                        <Button onClick={handleClose}>No</Button>
+                        <Button onClick={() => agree(item.ID)}>Yes</Button>
+                      </DialogActions>
+                    </Dialog>
+                    
+                  </>
                 )
               })}
             </TableBody>
